1b0b2bba58b308d3c78585cf1859c83e02c8c644
clwater <gengzhibo@gengzhibo.net>
2017-08-17 23:33:57 +0800

    20170817

--- app/src/main/java/clwater/androiddashboard/DashBoard.java
+++ app/src/main/java/clwater/androiddashboard/DashBoard.java
@@ -1,9 +1,11 @@
 package clwater.androiddashboard;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PaintFlagsDrawFilter;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.graphics.SweepGradient;
@@ -11,8 +13,8 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 
 /**
@@ -23,7 +25,8 @@ public class DashBoard extends View {
 
 
 
-    private Paint paint;
+    private Paint paint , tmpPaint , textPaint ,  strokePain;
+    private RectF rect;
     private int backGroundColor;    //背景色
     private float pointLength;      //指针长度
     private float per ;             //指数百分比
@@ -33,64 +36,54 @@ public class DashBoard extends View {
     private float r ;
 
 
-    //指针移动变化
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(android.os.Message msg) {
-            perPoint = perOld +  (float) msg.obj;
-            invalidate();
-        }
-    };
-
-    class SetIndex implements Runnable {
-
-        @Override
-        public void run() {
-            int count = 48; //比较流畅
-            for (int i = 0 ; i < count ; i ++ ){
-                try {
-                    Message message = Message.obtain();
-                    message.obj = (per - perOld) / count * ( i + 1) ;
-                    handler.sendMessage(message);
-                    Thread.sleep(1000 / count);
-                } catch (Exception e) {
-                    // TODO: handle exception
-                }
-            }
-
-        }
-    }
-
-
-
-
 
 
     public DashBoard(Context context) {
         super(context);
-        backGroundColor = Color.WHITE;
-        length = 300  / 4 * 3;
-
-
-        pointLength = 180;
-        per = 0;
-        perOld = 0;
-        r = 300;
+        init();
     }
 
 
 
     public DashBoard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int heitht = width / 2 / 4 * 5;
+        initIndex(width / 2);
+        //优化组件高度
+        setMeasuredDimension(width, heitht);
+    }
+
+
+
+    private void initIndex(int specSize) {
+        backGroundColor = Color.WHITE;
+        r = specSize;
+        length = r  / 4 * 3;
+        pointLength =  - (float) (r *  0.6);
+        per = 0;
+        perOld = 0;
+    }
+
+
+    private void init() {
+        paint = new Paint();
+        rect = new RectF();
+        textPaint = new Paint();
+        tmpPaint = new Paint();
+        strokePain = new Paint();
     }
 
     public DashBoard(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
-//    public void setLength(float length) {
-//        this.length = length;
-//    }
 
     public void setR(float r) {
         this.r = r;
@@ -100,48 +93,43 @@ public class DashBoard extends View {
     @Override
     protected void onDraw(Canvas canvas) {
 
-        paint = new Paint();
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
 
         //颜色指示的环
         initRing(canvas);
-
         //刻度文字
         initScale(canvas);
-
         //指针
         initPointer(canvas);
-
         //提示内容
         initText(canvas);
-
-
     }
 
     private void initText(Canvas canvas) {
+        //抗锯齿
+        canvas.setDrawFilter(new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG));
         canvas.restore();
         canvas.save();
         canvas.translate(canvas.getWidth()/2, r);
 
         float rIndex = length ;
-//        if (rIndex > 300) rIndex = 300;
 
-        //绘制圆盘阴影及圆盘
+        //设置文字展示的圆环
         paint.setColor(Color.parseColor("#eeeeee"));
         paint.setShader(null);
-        RectF rect = new RectF( - (rIndex/ 3 + rIndex/ 60), - (rIndex / 3 + rIndex/ 60) , rIndex / 3 + rIndex/ 60, rIndex / 3 + rIndex/ 60);
-        canvas.drawArc(rect, 0, 360, true, paint);
-
-        paint.setColor(Color.parseColor("#ffffff"));
-        paint.setShader(null);
+        paint.setShadowLayer(5, 0, 0, 0x54000000);
         rect = new RectF( - (rIndex/ 3 ), - (rIndex / 3), rIndex / 3, rIndex / 3);
         canvas.drawArc(rect, 0, 360, true, paint);
 
+        paint.clearShadowLayer();
+
         canvas.restore();
         canvas.save();
-        canvas.translate(canvas.getWidth()/2f  +  30   , r);
+        canvas.translate(canvas.getWidth()/2f , r);
 
-        Paint textPaint = new Paint();
+
         textPaint.setStrokeWidth(1);
+        textPaint.setAntiAlias(true);
 
         textPaint.setTextSize(60);
         textPaint.setColor(Color.parseColor("#fc6555"));
@@ -152,7 +140,6 @@ public class DashBoard extends View {
 
         int _per = (int) (per * 120);
 
-
         if (_per < 60){
             textPaint.setColor(Color.parseColor("#ff6450"));
         }else if (_per < 100) {
@@ -161,22 +148,25 @@ public class DashBoard extends View {
             textPaint.setColor(Color.parseColor("#79d062"));
         }
 
+        float swidth = textPaint.measureText(String.valueOf(_per));
+        //计算偏移量 是的数字和百分号整体居中显示
+        swidth =   (swidth - (swidth + 22) / 2);
 
+
+        canvas.translate( swidth , 0);
         canvas.drawText("" + _per, 0, 0, textPaint);
 
         textPaint.setTextSize(30);
         textPaint.setTextAlign(Paint.Align.LEFT);
-        canvas.drawText("%" , 0, 0, textPaint);
 
+        canvas.drawText("%" , 0, 0, textPaint);
         textPaint.setTextAlign(Paint.Align.CENTER);
-        textPaint.setColor(Color.parseColor("#cecece"));
+        textPaint.setColor(Color.parseColor("#999999"));
 
 
         canvas.restore();
         canvas.save();
-        canvas.translate(canvas.getWidth()/2 + rIndex/3f /6f , r);
-
-        canvas.translate(- rIndex/3f /6f,40);
+        canvas.translate(canvas.getWidth()/2  , r + length / 3 /2 );
         canvas.drawText("完成率" , 0, 0, textPaint);
 
 
@@ -196,8 +186,9 @@ public class DashBoard extends View {
         canvas.restore();
         canvas.save();
         canvas.translate(canvas.getWidth()/2, r);
-        paint.setColor(Color.BLACK);
-        Paint tmpPaint = new Paint(paint); //小刻度画笔对象
+        paint.setColor(Color.parseColor("#999999"));
+
+        tmpPaint = new Paint(paint); //小刻度画笔对象
         tmpPaint.setStrokeWidth(1);
         tmpPaint.setTextSize(35);
         tmpPaint.setTextAlign(Paint.Align.CENTER);
@@ -235,8 +226,6 @@ public class DashBoard extends View {
     private void initPointer(Canvas canvas) {
         paint.setColor(Color.BLACK);
 
-        Paint pointerPaint = new Paint(paint);
-        pointerPaint.setStrokeWidth(1);
 
         canvas.restore();
         canvas.save();
@@ -255,71 +244,78 @@ public class DashBoard extends View {
         //绘制三角形形成指针
         Path path = new Path();
         path.moveTo(0 , pointLength);
-        path.lineTo(-15 , 0);
-        path.lineTo(15,0);
+        path.lineTo(-10 , 0);
+        path.lineTo(10,0);
         path.lineTo(0 , pointLength);
         path.close();
+
         canvas.drawPath(path, paint);
 
     }
 
     private void initRing(Canvas canvas) {
-
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(2);
         canvas.save();
         canvas.translate(canvas.getWidth()/2, r);
 
-        //圆环外侧描边
-        paint.setColor(Color.parseColor("#979797"));
-        RectF rect = new RectF( -length - 1, -length - 1, length + 1, length + 1);
-        canvas.drawArc(rect, 175, 190, true, paint);
 
         //前100红黄渐变圆环
+        paint.setStyle(Paint.Style.FILL);
         int[] colors = {Color.parseColor("#F95A37"), Color.parseColor("#f9cf45"), Color.parseColor("#00ff00")};
-        float[] positions = {0.5f - 5f/180f * 0.5f, 0.5f + 0.5f * 5f / 6f, 1.0f};
+        float[] positions = {0.5f - 10f/180f * 0.5f, 0.5f + 0.5f * 5f / 6f, 1.0f};
         SweepGradient sweepGradient = new SweepGradient(0, 0, colors, positions);
         paint.setShader(sweepGradient);
         rect = new RectF( -length, -length, length, length);
-        canvas.drawArc(rect, 175, 5f + 180f / 6f * 5f, true, paint);
+        canvas.drawArc(rect, 170, 10f + 180f / 6f * 5f, true, paint);
 
-
-        canvas.rotate(5,0f,0f);
-        int[] colors2 = {Color.parseColor("#79D062"),  Color.parseColor("#3FBF55")};
-        float[] positions2 = {0.5f + 0.5f * ( 145f / 180f), 1.0f};
 
 
         //100之后绿色渐变圆环
+        paint.setStyle(Paint.Style.FILL);
+        canvas.rotate(10,0f,0f);
+        int[] colors2 = {Color.parseColor("#79D062"),  Color.parseColor("#3FBF55")};
+        float[] positions2 = {0.5f + 0.5f * ( 144f / 180f), 1.0f};
         sweepGradient = new SweepGradient(0, 0, colors2, positions2);
         paint.setShader(sweepGradient);
         rect = new RectF( -length, -length, length, length);
-        canvas.drawArc(rect, 180f + 180f * (145f / 180f), 180f / 6 + 5, true, paint);
+        canvas.drawArc(rect, 180f + 180f * (140f / 180f), 180f / 6 + 10, true, paint);
 
-        canvas.rotate(-5,0f,0f);
-        //圆环内侧描边
-        paint.setShader(null);
-        paint.setColor(Color.parseColor("#979797"));
-        rect = new RectF( - (length / 3f * 2f  ), -(length / 3f * 2f), length / 3f  * 2f, length / 3f * 2f);
-        canvas.drawArc(rect, 175, 190, true, paint);
-
-
-        //内部背景色填充
-        paint.setColor(backGroundColor);
-        paint.setShader(null);
-        rect = new RectF( - (length - length / 3f  - 1), -(length / 3f * 2f - 1), length - length / 3f -1 , length / 3f * 2f - 1);
-        canvas.drawArc(rect, 174, 192, true, paint);
 
 
         canvas.restore();
         canvas.save();
         canvas.translate(canvas.getWidth()/2, r);
 
+        strokePain = new Paint(paint);
 
-        //圆环两侧描边
-        paint.setColor(Color.BLACK);
-        canvas.rotate(85 ,0f , 0f);
-        canvas.drawLine(0f, 0 , 0, length / 3, paint);
+        strokePain.setColor(0x3f979797);
+        strokePain.setStrokeWidth(10);
+        strokePain.setShader(null);
+        strokePain.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(rect, 170, 200, true, strokePain);
 
-        canvas.rotate(-85 + -85  ,0f , 0f);
-        canvas.drawLine(0f, 0 , 0, length / 3 , paint);
+
+
+        canvas.restore();
+        canvas.save();
+        canvas.translate(canvas.getWidth()/2, r);
+
+        //底边水平
+        paint.setShader(null);
+        paint.setColor(backGroundColor);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(-length  , (float) (Math.sin(Math.toRadians(10) ) * length /3f * 2f), length  ,  (float) (Math.sin(Math.toRadians(10)) * length  + 100) , paint);
+        canvas.drawRect(-length  , (float) (Math.sin(Math.toRadians(10) ) * length /3f * 2f), length  ,  (float) (Math.sin(Math.toRadians(10) ) * length /3f * 2f) , strokePain);
+
+
+        //内部背景色填充
+        paint.setColor(backGroundColor);
+        paint.setShader(null);
+        rect = new RectF( - (length - length / 3f  - 2), -(length / 3f * 2f - 2), length - length / 3f -2 , length / 3f * 2f - 2);
+        canvas.drawArc(rect, 170, 200, true, strokePain);
+        canvas.drawArc(rect, 0, 360, true, paint);
+
 
 
     }
@@ -329,7 +325,17 @@ public class DashBoard extends View {
     public void cgangePer(float per ){
         this.perOld = this.per;
         this.per = per;
-        new Thread(new SetIndex()).start();
+        ValueAnimator va =  ValueAnimator.ofFloat(perOld,per);
+        va.setDuration(1000);
+        va.setInterpolator(new OvershootInterpolator());
+        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                perPoint = (float) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        va.start();
 
     }
 }
