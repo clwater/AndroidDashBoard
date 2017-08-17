620a1994bf312b65820e200a089117b899e2baf9
clwater <gengzhibo@gengzhibo.net>
2017-08-17 14:50:39 +0800

    20170817

--- app/src/main/java/clwater/androiddashboard/MainActivity.java
+++ app/src/main/java/clwater/androiddashboard/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         final DashBoard d = findViewById(R.id.dash);
 
-        
+
         findViewById(R.id.rand).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
